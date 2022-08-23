bef3f8aba98b030a2cc33110d22bf03365d9f8f5
VictorKalantay <v.kalantay@ukr.net>
2022-08-24 00:43:09 +0300

    Four changes 1) fix: Fixed a bug when clicking on a phone number. The call function now runs correctly 🐛 2) fix: In some popup the default action was not removed 🐛. Fixed 3) perf: Replaced slider. Now the project uses the same type of code (slider) ✨ 4) perf: Change content in Products-block ✨

--- js/script.js
+++ js/script.js
@@ -25,7 +25,6 @@ if (navLinks.length > 0) { //если такие есть, то
     navLink.addEventListener("click", onNavLinkClick); //вешием событие каждому
   });
 }
-
 function onNavLinkClick(e) { //создаем функцию клика
   const navLink = e.target; //получаем целевой объект по клику
   if (navLink.dataset.goto && document.querySelector(navLink.dataset.goto)) { //проверяем заполнен ли дата атрибут и есть ли секция
@@ -63,6 +62,7 @@ popupLinkButton.forEach((el) => {
 });
 document.addEventListener("click", (e) => {
   if (e.target.closest("[data-close]")) {
+    e.preventDefault()
     closePopup()
    }
   if (!e.target.closest('.popup__content') && e.target.closest("[data-closeArea]")) {
@@ -77,133 +77,133 @@ document.addEventListener('keydown', (e) => {
 
 
 // Слайдер  по примеру. Вычистил лишний функционал но надо разбираться
-function Slider(sldrId) {  // Проверка наличия ID для возможности вставки нескольких слайдеров на страницу
-
-  let id = document.getElementById(sldrId);
-  if (id) {
-    this.sldrRoot = id; // Если есть - работаем с айди
-  } else {
-    this.sldrRoot = document.querySelector('.services-popup__carousel-block') // нет - по селектору
-  }
-
-
-  // Carousel objects
-  this.sldrList = this.sldrRoot.querySelector('.slider-list'); // берем список слайдов
-  this.sldrElements = this.sldrList.querySelectorAll('.slider-element'); // из него список элементов
-  this.sldrElemFirst = this.sldrList.querySelector('.slider-element'); // первый слайд
-  this.indicatorDots = this.sldrRoot.querySelector('div.slider-dots'); // контейнер точек
-  // Initialization
-  this.options = Slider.defaults;
-  Slider.initialize(this)
-
-
-}
-
-Slider.defaults = {
-  // Default options for the carousel
-  loop: true,     // Бесконечное зацикливание слайдера
-  auto: true,     // Автоматическое пролистывание
-  interval: 3000, // Интервал между пролистыванием элементов (мс)
-  dots: true      // Индикаторные точки
-};
-Slider.prototype.elemPrev = function (num) {
-  num = num || 1; // если num вернет false то присвоем num 1
-  let prevElement = this.currentElement;
-
-  this.currentElement -= num;
-  if (this.currentElement < 0) this.currentElement = this.elemCount - 1;
-  this.sldrElements[this.currentElement].style.opacity = '1';
-  this.sldrElements[prevElement].style.opacity = '0';
-  if (this.options.dots) {
-    this.dotOn(prevElement);
-    this.dotOff(this.currentElement)
-  }
-};
-Slider.prototype.elemNext = function (num) {
-  num = num || 1;
-  let prevElement = this.currentElement;
-  this.currentElement += num;
-  if (this.currentElement >= this.elemCount) this.currentElement = 0;
-  this.sldrElements[this.currentElement].style.opacity = '1';
-  this.sldrElements[prevElement].style.opacity = '0';
-  if (this.options.dots) {
-    this.dotOn(prevElement);
-    this.dotOff(this.currentElement)
-  }
-};
-Slider.prototype.dotOn = function (num) {
-  this.indicatorDotsAll[num].style.cssText = 'background-color:#fff; cursor:pointer;'
-};
-Slider.prototype.dotOff = function (num) {
-  this.indicatorDotsAll[num].style.cssText = 'background-color:#EF7F1A; cursor:default;'
-};
-Slider.initialize = function (that) {
-  // Constants
-  that.elemCount = that.sldrElements.length; // Количество элементов
-  // Variables
-  that.currentElement = 0;
-  let bgTime = getTime();
-
-  // Functions
-  function getTime() {
-    return new Date().getTime();
-  }
-
-  function setAutoScroll() {
-    that.autoScroll = setInterval(function () {
-      let fnTime = getTime();
-      if (fnTime - bgTime + 10 > that.options.interval) {
-        bgTime = fnTime;
-        that.elemNext()
-      }
-    }, that.options.interval)
-  }
-
-  // Start initialization
-  if (that.elemCount >= 1) {   // показать первый элемент
-    that.sldrElemFirst.style.opacity = '1';
-  }
-  if (!that.options.loop) {
-    that.options.auto = false; // отключить автопркрутку
-  } else if (that.options.auto) {   // инициализация автопрокруки
-    setAutoScroll();
-    // Остановка прокрутки при наведении мыши на элемент
-    that.sldrList.addEventListener('mouseenter', function () {
-      clearInterval(that.autoScroll)
-    }, false);
-    that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
-
-
-  }
-
-
-  if (that.options.dots) {  // инициализация индикаторных точек
-    let sum = '', diffNum;
-    for (let i = 0; i < that.elemCount; i++) {
-      sum += '<span class="dot"></span>'
-    }
-    that.indicatorDots.innerHTML = sum;
-    that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.dot');
-    // Назначаем точкам обработчик события 'click'
-    for (let n = 0; n < that.elemCount; n++) {
-      that.indicatorDotsAll[n].addEventListener('click', function () {
-        diffNum = Math.abs(n - that.currentElement);
-        if (n < that.currentElement) {
-          bgTime = getTime();
-          that.elemPrev(diffNum)
-        } else if (n > that.currentElement) {
-          bgTime = getTime();
-          that.elemNext(diffNum)
-        }
-        // Если n == that.currentElement ничего не делаем
-      }, false)
-    }
-    that.dotOff(0);  // точка[0] выключена, остальные включены
-    for (let i = 1; i < that.elemCount; i++) {
-      that.dotOn(i)
-    }
-  }
-}
+// function Slider(sldrId) {  // Проверка наличия ID для возможности вставки нескольких слайдеров на страницу
+//
+//   let id = document.getElementById(sldrId);
+//   if (id) {
+//     this.sldrRoot = id; // Если есть - работаем с айди
+//   } else {
+//     this.sldrRoot = document.querySelector('.services-popup__carousel-block') // нет - по селектору
+//   }
+//
+//
+//   // Carousel objects
+//   this.sldrList = this.sldrRoot.querySelector('.slider-list'); // берем список слайдов
+//   this.sldrElements = this.sldrList.querySelectorAll('.slider-element'); // из него список элементов
+//   this.sldrElemFirst = this.sldrList.querySelector('.slider-element'); // первый слайд
+//   this.indicatorDots = this.sldrRoot.querySelector('div.slider-dots'); // контейнер точек
+//   // Initialization
+//   this.options = Slider.defaults;
+//   Slider.initialize(this)
+//
+//
+// }
+//
+// Slider.defaults = {
+//   // Default options for the carousel
+//   loop: true,     // Бесконечное зацикливание слайдера
+//   auto: true,     // Автоматическое пролистывание
+//   interval: 3000, // Интервал между пролистыванием элементов (мс)
+//   dots: true      // Индикаторные точки
+// };
+// Slider.prototype.elemPrev = function (num) {
+//   num = num || 1; // если num вернет false то присвоем num 1
+//   let prevElement = this.currentElement;
+//
+//   this.currentElement -= num;
+//   if (this.currentElement < 0) this.currentElement = this.elemCount - 1;
+//   this.sldrElements[this.currentElement].style.opacity = '1';
+//   this.sldrElements[prevElement].style.opacity = '0';
+//   if (this.options.dots) {
+//     this.dotOn(prevElement);
+//     this.dotOff(this.currentElement)
+//   }
+// };
+// Slider.prototype.elemNext = function (num) {
+//   num = num || 1;
+//   let prevElement = this.currentElement;
+//   this.currentElement += num;
+//   if (this.currentElement >= this.elemCount) this.currentElement = 0;
+//   this.sldrElements[this.currentElement].style.opacity = '1';
+//   this.sldrElements[prevElement].style.opacity = '0';
+//   if (this.options.dots) {
+//     this.dotOn(prevElement);
+//     this.dotOff(this.currentElement)
+//   }
+// };
+// Slider.prototype.dotOn = function (num) {
+//   this.indicatorDotsAll[num].style.cssText = 'background-color:#fff; cursor:pointer;'
+// };
+// Slider.prototype.dotOff = function (num) {
+//   this.indicatorDotsAll[num].style.cssText = 'background-color:#EF7F1A; cursor:default;'
+// };
+// Slider.initialize = function (that) {
+//   // Constants
+//   that.elemCount = that.sldrElements.length; // Количество элементов
+//   // Variables
+//   that.currentElement = 0;
+//   let bgTime = getTime();
+//
+//   // Functions
+//   function getTime() {
+//     return new Date().getTime();
+//   }
+//
+//   function setAutoScroll() {
+//     that.autoScroll = setInterval(function () {
+//       let fnTime = getTime();
+//       if (fnTime - bgTime + 10 > that.options.interval) {
+//         bgTime = fnTime;
+//         that.elemNext()
+//       }
+//     }, that.options.interval)
+//   }
+//
+//   // Start initialization
+//   if (that.elemCount >= 1) {   // показать первый элемент
+//     that.sldrElemFirst.style.opacity = '1';
+//   }
+//   if (!that.options.loop) {
+//     that.options.auto = false; // отключить автопркрутку
+//   } else if (that.options.auto) {   // инициализация автопрокруки
+//     setAutoScroll();
+//     // Остановка прокрутки при наведении мыши на элемент
+//     that.sldrList.addEventListener('mouseenter', function () {
+//       clearInterval(that.autoScroll)
+//     }, false);
+//     that.sldrList.addEventListener('mouseleave', setAutoScroll, false)
+//
+//
+//   }
+//
+//
+//   if (that.options.dots) {  // инициализация индикаторных точек
+//     let sum = '', diffNum;
+//     for (let i = 0; i < that.elemCount; i++) {
+//       sum += '<span class="dot"></span>'
+//     }
+//     that.indicatorDots.innerHTML = sum;
+//     that.indicatorDotsAll = that.sldrRoot.querySelectorAll('span.dot');
+//     // Назначаем точкам обработчик события 'click'
+//     for (let n = 0; n < that.elemCount; n++) {
+//       that.indicatorDotsAll[n].addEventListener('click', function () {
+//         diffNum = Math.abs(n - that.currentElement);
+//         if (n < that.currentElement) {
+//           bgTime = getTime();
+//           that.elemPrev(diffNum)
+//         } else if (n > that.currentElement) {
+//           bgTime = getTime();
+//           that.elemNext(diffNum)
+//         }
+//         // Если n == that.currentElement ничего не делаем
+//       }, false)
+//     }
+//     that.dotOff(0);  // точка[0] выключена, остальные включены
+//     for (let i = 1; i < that.elemCount; i++) {
+//       that.dotOn(i)
+//     }
+//   }
+// }
 
 const questionsBlockDisclosure = () => {
   const questionsList = document.querySelectorAll('.questions__question')
@@ -220,11 +220,6 @@ const questionsBlockDisclosure = () => {
     })
   })
 }
-
-
-
-
-
 
 questionsBlockDisclosure()
 videoInsert()
--- js/slider_script.js
+++ js/slider_script.js
@@ -6,5 +6,52 @@ $(document).ready(function (){
     centerMode: true,
     variableWidth: true,
   });
-
+});
+$(document).ready(function (){
+  $('.services-slider--install').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
+});
+$(document).ready(function (){
+  $('.services-slider--loggias').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
+});
+$(document).ready(function (){
+  $('.services-slider--cottages').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
+});
+$(document).ready(function (){
+  $('.services-slider--corp').slick({
+    arrows: false,
+    dots: true,
+    slidesToShow: 1,
+    infinite: true,
+    autoplay:true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    pauseOnDotsHover: true,
+  });
 });
